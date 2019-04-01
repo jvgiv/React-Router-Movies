8b29dcb48973cf3764df5d597a6355b121107147
jvgiv <jvgiv12@gmail.com>
2019-04-01 18:06:01 -0400

    99% done

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import MovieList from './MovieList';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,7 +14,9 @@ export default class SavedList extends Component {
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
         ))}
-        <div className="home-button">Home</div>
+        <Link to='/' component={MovieList}>
+          <div className="home-button">Home</div>
+        </Link>
       </div>
     );
   }
